b25d2d055ed1a1042916416fee0ffdd75e71e5ff
ZerfyT <nipdili1143@gmail.com>
2023-04-06 20:04:21 +0530

    improve calculation functions.

--- meter_reading.php
+++ meter_reading.php
@@ -35,7 +35,6 @@ if (isset($_POST['bt_save'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Meter Reading</title>
-  <link rel="stylesheet" href="css/styles_meter_reading.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <link rel="stylesheet" href="css/styles.css">
